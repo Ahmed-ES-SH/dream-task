65adac285167c397e0aa322d5335d8b3722e54e3
Ahmed Ismail <133300134+Ahmed-ES-SH@users.noreply.github.com>
2026-08-01 19:10:02 -0700

    feat(home): redesign hero with CTAs and role list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,5 @@
-import { useTranslations } from "../hooks/useTranslations";
+import HeroSection from "../components/website/HeroSection";
 
 export default function Home() {
-  const t = useTranslations();
-
-  return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <h1>{t("home.title")}</h1>
-    </div>
-  );
+  return <HeroSection />;
 }
